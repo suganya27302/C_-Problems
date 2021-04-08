ebdce8d15176c50d0756c50d55781d4af8fcdf74
suganya27302 <77053019+suganya27302@users.noreply.github.com>
2021-04-08 22:08:19 +0530

    Create square pattern.c

---
+++ square pattern.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    int n,i,j;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++){
+      for(j=1;j<=n;j++){
+          if(j==1||j==n||i==1||i==n){
+              printf("*");
+          }
+          else 
+              printf(" ");
+          }
+      }  
+                printf("\n");
+    }
+    return 0;
+}
